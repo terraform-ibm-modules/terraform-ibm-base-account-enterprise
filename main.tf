e95739fa196e9c826e6bca7cd8ad410aea7b1f2c
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-08-10 07:32:49 +0000

    fix(deps): update terraform terraform-ibm-modules/iam-account-settings/ibm to v2.10.5

--- main.tf
+++ main.tf
@@ -163,7 +163,7 @@ moved {
 module "account_settings" {
   count                        = !var.skip_iam_account_settings ? 1 : 0
   source                       = "terraform-ibm-modules/iam-account-settings/ibm"
-  version                      = "2.10.4"
+  version                      = "2.10.5"
   access_token_expiration      = var.access_token_expiration
   active_session_timeout       = var.active_session_timeout
   allowed_ip_addresses         = var.allowed_ip_addresses
