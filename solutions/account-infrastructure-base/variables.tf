0bf174d38f1d4daa9bd7ca846e19d070694360df
kierra.searle@ibm.com <kierra.searle@ibm.com>
2024-11-15 10:50:22 -0500

    fix: added coalesces

--- solutions/account-infrastructure-base/variables.tf
+++ solutions/account-infrastructure-base/variables.tf
@@ -24,7 +24,7 @@ variable "prefix" {
   # this value was determined based on the lowest prefix restriction located here:
   # https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone/blob/main/patterns/roks/variables.tf#L11
   validation {
-    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(coalesce(var.prefix, " ")) <= 13
+    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", coalesce(var.prefix, "cos"))) && length(coalesce(var.prefix, "cos")) <= 13
     error_message = "Prefix must begin with a lowercase letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and length must be 13 characters or less."
   }
 }
